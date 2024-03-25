c463b9253a17494e3b51bc0404931327eb046626
sanath2567 <sriramsanath555@gmail.com>
2024-03-25 15:07:25 +0530

    for

--- main.js
+++ main.js
@@ -58,3 +58,24 @@ document.querySelectorAll('.portfolio-box').forEach(box => {
     window.location.href = box.querySelector('a').href; // Redirect to the link
   });
 });
+
+// dummy
+
+const inputs = document.querySelectorAll(".input");
+
+function focusFunc() {
+  let parent = this.parentNode;
+  parent.classList.add("focus");
+}
+
+function blurFunc() {
+  let parent = this.parentNode;
+  if (this.value == "") {
+    parent.classList.remove("focus");
+  }
+}
+
+inputs.forEach((input) => {
+  input.addEventListener("focus", focusFunc);
+  input.addEventListener("blur", blurFunc);
+});
